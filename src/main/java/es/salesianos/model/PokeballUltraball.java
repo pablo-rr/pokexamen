31ec308d00ec238d1c1d0b6ce94c4ff3d1ea1b8c
pablo-rr <pablorr2gmail.com>
2020-02-18 10:25:06 +0100

    added new pokeball profiles
    Signed-off-by: pablo-rr <pablorr2gmail.com>

--- src/main/java/es/salesianos/model/PokeballUltraball.java
+++ src/main/java/es/salesianos/model/PokeballUltraball.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component("pokeball")
-@Profile("pokeballBasic")
-public class PokeballBasic extends AbstractPokeball{
-	public PokeballBasic() {
-		setCatchChancePercentage(10);
+@Profile("pokeballUltraball")
+public class PokeballUltraball extends AbstractPokeball{
+	public PokeballUltraball() {
+		setCatchChancePercentage(50);
 	}
 }
